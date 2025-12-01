2645f95ad3d97faa933c5fadb6f9beb99cd96036
jose lazaro <marqueslazaro13@gmail.com>
2025-12-01 16:24:34 -0400

    versão inicial de estilização do texto

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,45 @@
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import TextField from "@mui/material/TextField";
-import { useState } from "react";
+import { useRef, useState } from "react";
+import Button from "@mui/material/Button";
 
 export default function Home() {
   const [markdown, setMarkdown] = useState<string>("");
+  const ref = useRef<HTMLTextAreaElement | null>(null);
+
+  const toTitle = (text: string) => `# ${text}`;
+  const toBold = (text: string) => `**${text}**`;
+  const toItalic = (text: string) => `*${text}*`;
+
+  const selectAndEditText = (editFn: (text: string) => string) => {
+    return () => {
+      const textArea = ref.current;
+
+      const start = textArea?.selectionStart;
+      const end = textArea?.selectionEnd;
+
+      const selectedText = markdown.slice(start, end);
+      const editedText = editFn(selectedText);
+
+      const newMarkdown =
+        markdown.slice(0, start) + editedText + markdown.slice(end);
+
+      setMarkdown(newMarkdown);
+    };
+  };
 
   return (
     <div className="prose">
+      <Button variant="outlined" onClick={selectAndEditText(toTitle)}>
+        Título
+      </Button>
+      <Button variant="outlined" onClick={selectAndEditText(toBold)}>
+        Negrito
+      </Button>
+      <Button variant="outlined" onClick={selectAndEditText(toItalic)}>
+        Italíco
+      </Button>
       <TextField
         label="Escreva Aqui"
         multiline
@@ -16,6 +48,7 @@ export default function Home() {
         fullWidth
         value={markdown}
         onChange={(e) => setMarkdown(e.target.value)}
+        inputRef={ref}
       />
       <ReactMarkdown remarkPlugins={[remarkGfm]}>{markdown}</ReactMarkdown>
     </div>
