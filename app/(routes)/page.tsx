8c1fc1ca25075fbc7dd63c8af21d5635941f38eb
jose lazaro <marqueslazaro13@gmail.com>
2025-12-05 16:17:40 -0400

    componentização da listagem de documentos

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,28 +1,14 @@
 "use client";
 
+import DocumentList from "../components/DocumentList/DocumentList";
 import { useDocumentsContext } from "../context/DocumentsContext";
-import { useRouter } from "next/navigation";
-
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemText from "@mui/material/ListItemText";
-import { ListItemButton } from "@mui/material";
 
 export default function Home() {
-  const router = useRouter();
   const { documents } = useDocumentsContext();
 
   return (
     <>
-      <List>
-        {documents.map((doc) => (
-          <ListItem key={doc.id}>
-            <ListItemButton onClick={() => router.push(`/${doc.id}`)}>
-              <ListItemText primary={doc.title} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
+      <DocumentList documents={documents} />
     </>
   );
 }
