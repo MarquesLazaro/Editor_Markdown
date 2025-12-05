eb4450db23f2de9d78459fd4852de32de09ed34b
jose lazaro <marqueslazaro13@gmail.com>
2025-12-05 18:56:35 -0400

    feature de criação de novo documento

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -2,6 +2,7 @@
 
 import DocumentList from "../components/DocumentList/DocumentList";
 import { useDocumentsContext } from "../context/DocumentsContext";
+import { Button } from "@mui/material";
 
 export default function Home() {
   const { documents } = useDocumentsContext();
